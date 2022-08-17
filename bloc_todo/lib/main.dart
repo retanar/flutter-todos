c56f193fd0c37fee6efc433bef3417df8de9fec0
Retanar <59249123+retanar@users.noreply.github.com>
2022-08-17 17:17:13 +0300

    Mistake in name

--- bloc_todo/lib/main.dart
+++ bloc_todo/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TODO MVP',
+      title: 'TODO BLOC',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
